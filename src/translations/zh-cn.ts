345e18ad92fe244fea3e0d3a14cb02365ddacfc8
ypingcn <1344632698@qq.com>
2017-08-19 14:41:59 +0800

    [translation] finish translations

--- src/translations/zh-cn.ts
+++ src/translations/zh-cn.ts
@@ -1,248 +1,251 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="zh_CN">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../mainwindow.ui" line="26"/>
         <source>Chat</source>
-        <translation type="unfinished"></translation>
+        <translation>聊天</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="42"/>
         <source>NickName:</source>
-        <translation type="unfinished"></translation>
+        <translation>昵称:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="68"/>
         <source>Your Address:</source>
-        <translation type="unfinished"></translation>
+        <translation>您的地址:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="94"/>
         <source>Login</source>
-        <translation type="unfinished"></translation>
+        <translation>登录</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="108"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>退出</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="139"/>
         <source>OnlineUsers:</source>
-        <translation type="unfinished"></translation>
+        <translation>在线用户:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="156"/>
         <source>IP args</source>
-        <translation type="unfinished"></translation>
+        <translation>IP 地址参数</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="184"/>
         <source>Send</source>
-        <translation type="unfinished"></translation>
+        <translation>发送</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="246"/>
         <source>Send Message</source>
-        <translation type="unfinished"></translation>
+        <translation>发送信息</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="269"/>
         <source>Mask</source>
-        <translation type="unfinished"></translation>
+        <translation>登录模式</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="277"/>
         <source>255.255.255.255</source>
-        <translation type="unfinished"></translation>
+        <translation>255.255.255.255</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="282"/>
         <source>192.168.255.255</source>
-        <translation type="unfinished"></translation>
+        <translation>192.168.255.255</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="302"/>
         <location filename="../mainwindow.cpp" line="167"/>
         <location filename="../mainwindow.cpp" line="323"/>
         <source>Listen</source>
-        <translation type="unfinished"></translation>
+        <translation>监听</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="313"/>
         <source>Choose</source>
-        <translation type="unfinished"></translation>
+        <translation>选择</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="336"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>编辑</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="340"/>
         <location filename="../mainwindow.cpp" line="81"/>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>语言设置</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="350"/>
         <location filename="../mainwindow.ui" line="376"/>
         <location filename="../mainwindow.cpp" line="100"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>帮助</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="361"/>
         <source>English</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="366"/>
         <source>简体中文</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="371"/>
         <source>繁體中文</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="381"/>
         <location filename="../mainwindow.cpp" line="91"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>关于</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="81"/>
         <source>Restart the app to switch language</source>
-        <translation type="unfinished"></translation>
+        <translation>重启应用完成语言更改</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="93"/>
         <source>click to know more.</source>
-        <translation type="unfinished"></translation>
+        <translation>点击这里了解更多信息。</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="94"/>
         <source>A simple program designed for LAN chat.</source>
-        <translation type="unfinished"></translation>
+        <translation>一个旨在提供局域网聊天的小工具.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="103"/>
         <source>P2PChat-Qt Help</source>
-        <translation type="unfinished"></translation>
+        <translation>P2PChat-Qt 帮助</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="104"/>
         <source>
  --- Nick Name Help</source>
-        <translation type="unfinished"></translation>
+        <translation>
+ --- 昵称部分</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="105"/>
         <source>Valid character include : numbers , alphabet ( lower-case and upper-case ) , underline and simplified chinese character.</source>
-        <translation type="unfinished"></translation>
+        <translation>有效字符包括：数字，英文字母（大小写），下划线和简体中文字符。</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="106"/>
         <source>
  --- Mask Help</source>
-        <translation type="unfinished"></translation>
+        <translation>
+ --- 登录模式部分</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="107"/>
         <source>Mask help you to chat in different range.</source>
-        <translation type="unfinished"></translation>
+        <translation>登录模式使你能在不同的范围内聊天。</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="108"/>
         <source>#255.255.255.255 option# local usage , Presentation mode</source>
-        <translation type="unfinished"></translation>
+        <translation>#255.255.255.255 选项# 本地使用，演示模式</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="109"/>
         <source>#192.168.1.1 option# LAN usage , if two clients whose IP begins with 192.168 , are in the same LAN</source>
-        <translation type="unfinished"></translation>
+        <translation>#192.168.255.255 选项# 局域网用，如果两个用户均在同一个局域网且IP地址均以192.168开头</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="110"/>
         <source>
  --- IP Args Help</source>
-        <translation type="unfinished"></translation>
+        <translation>
+ --- IP地址参数部分</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="111"/>
         <source>two number mean IP address and port number.</source>
-        <translation type="unfinished"></translation>
+        <translation>两个数字代表着 IP 地址和端口号。</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="112"/>
         <source>You should input an vaild IP address in the first blank ( four numbers, separated by dots and ranged between 0 and 255 )</source>
-        <translation type="unfinished"></translation>
+        <translation>在第一个空里填入一个有效的 IP 地址（在0到255之间的四个数字且以点分隔开）</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="113"/>
         <source>#127.0.0.1 is Presentation mode, you should input the destination IP instead.( As OnlineUsers shows )</source>
-        <translation type="unfinished"></translation>
+        <translation>#127.0.0.1 是演示模式，你得填入文件传输的目的地IP（正如在线用户列表里显示的那样）</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="114"/>
         <source>Number in the second blank should be a number ranged between 1 and 65535</source>
-        <translation type="unfinished"></translation>
+        <translation>第二个空里的数字得是在1到65535之间的数字</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="115"/>
         <source>Port number don&apos;t recommand to change unless the two clients use the same port for file transmisson.</source>
-        <translation type="unfinished"></translation>
+        <translation>端口号不推荐修改，除非两个用户之间使用相同的端口来传输文件。</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="116"/>
         <source>&apos;Listen&apos; means I am ready to get the file.</source>
-        <translation type="unfinished"></translation>
+        <translation>点击“监听”意味着我准备好接受文件了。</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="228"/>
         <source> -- enter the chat room</source>
-        <translation type="unfinished"></translation>
+        <translation> -- 进入了聊天室</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="241"/>
         <source> -- quit the chat room</source>
-        <translation type="unfinished"></translation>
+        <translation> -- 退出了聊天室</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="259"/>
         <source>No message</source>
-        <translation type="unfinished"></translation>
+        <translation>无信息</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="271"/>
         <source>Invaild Nickname!</source>
-        <translation type="unfinished"></translation>
+        <translation>无效昵称！</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="273"/>
         <location filename="../mainwindow.cpp" line="287"/>
         <source>Offline</source>
-        <translation type="unfinished"></translation>
+        <translation>离线</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="273"/>
         <location filename="../mainwindow.cpp" line="287"/>
         <source>Check your Network to login</source>
-        <translation type="unfinished"></translation>
+        <translation>检查您的网络以登录</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="299"/>
         <source>Choose File</source>
-        <translation type="unfinished"></translation>
+        <translation>选择文件</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="299"/>
         <source>All File(*.*)</source>
-        <translation type="unfinished"></translation>
+        <translation>所有文件(*.*)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="306"/>
@@ -254,61 +257,61 @@
         <location filename="../mainwindow.cpp" line="455"/>
         <location filename="../mainwindow.cpp" line="471"/>
         <source>System</source>
-        <translation type="unfinished"></translation>
+        <translation>系统提示</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="306"/>
         <source> -- File Selete: %1</source>
-        <translation type="unfinished"></translation>
+        <translation> -- 选择了文件：%1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="316"/>
         <source>UnListen</source>
-        <translation type="unfinished"></translation>
+        <translation>取消监听</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="319"/>
         <source> -- File Port Listening</source>
-        <translation type="unfinished"></translation>
+        <translation> -- 正在监听文件传输端口</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="326"/>
         <source> -- File Port Listening Closed</source>
-        <translation type="unfinished"></translation>
+        <translation> -- 取消监听文件传输端口</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="333"/>
         <source> -- New File Arriving</source>
-        <translation type="unfinished"></translation>
+        <translation> -- 新文件到达</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="382"/>
         <location filename="../mainwindow.cpp" line="455"/>
         <source> -- File Name: %1 File Size: %2</source>
-        <translation type="unfinished"></translation>
+        <translation> -- 文件名:%1 文件大小:%2</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="397"/>
         <location filename="../mainwindow.cpp" line="471"/>
         <source> -- File Transmission Complete</source>
-        <translation type="unfinished"></translation>
+        <translation> -- 文件传输完毕</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="400"/>
         <source>Open File Folder?</source>
-        <translation type="unfinished"></translation>
+        <translation>打开文件夹？</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="427"/>
         <location filename="../mainwindow.cpp" line="462"/>
         <source>ERROR</source>
-        <translation type="unfinished"></translation>
+        <translation>错误</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="427"/>
         <location filename="../mainwindow.cpp" line="462"/>
         <source>Network Error</source>
-        <translation type="unfinished"></translation>
+        <translation>网络错误</translation>
     </message>
 </context>
 </TS>
